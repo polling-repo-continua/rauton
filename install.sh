a82193e12f0e2d9202debc6a4cba382a6efab8c0
root <root@Huntinex.localdomain>
2021-10-12 11:12:42 +0330

    s

--- install.sh
+++ install.sh
@@ -9,10 +9,11 @@ apt install sslscan
 
 mkdir $1
 
-
 git clone https://github.com/michenriksen/aquatone $1/aquatone
 git clone https://github.com/projectdiscovery/nuclei $1/nuclei
 git clone https://github.com/tomnomnom/hacks/ $1/hacks
 git clone https://github.com/tomnomnom/anew $1/anew
 git clone https://github.com/tomnomnom/gf $1/gf
 git clone https://github.com/003random/getJS $1/getjs
+
+echo "Finished !"
